1952e6a81170a4a008f6964057e6961da9884c76
W-Sikora <242612@student.pwr.edu.pl>
2022-11-26 16:02:09 +0100

    konfiguracja wstępna

---
+++ src/test/java/pl/wsikora/budget/BudgetApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.wsikora.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
